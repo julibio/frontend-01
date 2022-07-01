468436d237ae3aecb781bef2f0f5be9ba9dc0bfa
Julibio <julibio@hotmail.com>
2022-07-01 08:44:22 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <MainPage/>
+</template>
+
+<script>
+import MainPage from "./views/MainPage.vue"
+
+export default {
+  name: 'App',
+  components: {
+    MainPage
+  }
+}
+</script>
+
+<style src="./assets/style.css"/>
+
